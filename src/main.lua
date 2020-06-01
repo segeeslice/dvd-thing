f1cb3840f6834bed5cfa761ee7c852174e930362
Dustin Seger <dustin.seger@hotmail.com>
2020-06-01 12:08:37 +0000

    Implement bouncing rectangle

--- src/main.lua
+++ src/main.lua
@@ -1,14 +1,48 @@
-NUM = 0
+WIN = {
+  x = {
+    size = 0
+  },
+  y = {
+    size = 0
+  }
+}
+
+rect = {
+  x = {
+    position = 0,
+    size = 100,
+    shouldInc = true
+  },
+  y = {
+    position = 0,
+    size = 100,
+    shouldInc = true
+  }
+}
+
+function love.load()
+  WIN.x.size, WIN.y.size = love.graphics.getDimensions()
+  print(WIN.x.size, WIN.y.size)
+end
 
 function love.draw()
-  str = string.format('The number is now %d', NUM)
-  love.graphics.print(str, 300, 300)
+  love.graphics.rectangle('fill', rect.x.position, rect.y.position, 100, 100)
 end
 
 function love.update(dt)
-  if love.keyboard.isDown('up') then
-    NUM = NUM + 100 * dt
-  elseif love.keyboard.isDown('down') then
-    NUM = NUM - 100 * dt
+  local incVal = 100 * dt
+
+  for k,o in pairs(rect) do
+    if o.shouldInc == true and o.position + o.size + incVal > WIN[k].size then
+      o.shouldInc = false
+    elseif o.shouldInc == false and o.position - incVal < 0 then
+      o.shouldInc = true
+    end
+
+    if o.shouldInc == true then
+      o.position = o.position + incVal
+    else
+      o.position = o.position - incVal
+    end
   end
 end
