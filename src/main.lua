5a0fee714f3d96820d900e9e855b670a4e395426
Dustin Seger <dustin.seger@hotmail.com>
2020-06-01 12:57:15 +0000

    Add DVD logo and organize main.lua

--- src/main.lua
+++ src/main.lua
@@ -1,3 +1,12 @@
+-- ** Global Definitions **
+
+-- Speed in pixels / sec
+LOGO_SPEED = 100
+
+DVD_LOGO_IMG = nil
+
+-- Global window object for more efficient storing of data
+-- TODO: allow resize and update this tracker
 WIN = {
   x = {
     size = 0
@@ -7,32 +16,32 @@ WIN = {
   }
 }
 
-rect = {
+-- Global logo data for x and y directions
+logo = {
   x = {
     position = 0,
-    size = 100,
+    size = 150,
     shouldInc = true
   },
   y = {
     position = 0,
-    size = 100,
+    size = 80,
     shouldInc = true
   }
 }
 
-function love.load()
-  WIN.x.size, WIN.y.size = love.graphics.getDimensions()
-  print(WIN.x.size, WIN.y.size)
+-- ** Util methods **
+-- TODO: Move logo stuff to own class in separate file
+
+function drawLogo ()
+  if DVD_LOGO_IMG == nil then return end
+  local scaleX = logo.x.size / DVD_LOGO_IMG:getWidth()
+  local scaleY = logo.y.size / DVD_LOGO_IMG:getHeight()
+  love.graphics.draw(DVD_LOGO_IMG, logo.x.position, logo.y.position, 0, scaleX, scaleY)
 end
 
-function love.draw()
-  love.graphics.rectangle('fill', rect.x.position, rect.y.position, 100, 100)
-end
-
-function love.update(dt)
-  local incVal = 100 * dt
-
-  for k,o in pairs(rect) do
+function incLogoPosition (incVal)
+  for k,o in pairs(logo) do
     if o.shouldInc == true and o.position + o.size + incVal > WIN[k].size then
       o.shouldInc = false
     elseif o.shouldInc == false and o.position - incVal < 0 then
@@ -45,4 +54,20 @@ function love.update(dt)
       o.position = o.position - incVal
     end
   end
+end
+
+-- ** Love callbacks **
+
+function love.load()
+  WIN.x.size, WIN.y.size = love.graphics.getDimensions()
+  DVD_LOGO_IMG = love.graphics.newImage('resources/dvd-logo.png')
+end
+
+function love.draw()
+  drawLogo()
+end
+
+function love.update(dt)
+  local incVal = LOGO_SPEED * dt
+  incLogoPosition(incVal)
 end
